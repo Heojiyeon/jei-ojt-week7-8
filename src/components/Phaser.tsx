3114a1004525fd513ad64120573349b99894d39f
Zena <33304871+Heojiyeon@users.noreply.github.com>
2024-02-01 09:47:14 +0900

    feat: phaser 캔버스 컴포넌트 구현

--- src/components/Phaser.tsx
+++ src/components/Phaser.tsx
@@ -1,5 +1,40 @@
+import { AUTO, Game, WEBGL } from 'phaser';
+import { useEffect, useRef } from 'react';
+
+/**
+ * @returns 페이저 캔버스 컴포넌트
+ */
 const Phaser = () => {
-  return <div>Phaser component</div>;
+  const PhaserGameRef = useRef<Game | null>(null);
+
+  useEffect(() => {
+    const gameArea = document.getElementById('game-area');
+
+    const config = {
+      type: WEBGL,
+      width: 1200,
+      height: 700,
+      AUTO,
+      gameArea,
+      physics: {
+        default: 'arcade',
+        arcade: {
+          gravity: { y: 300 },
+          debug: false,
+        },
+      },
+      scene: [],
+    };
+
+    PhaserGameRef.current = new Game(config);
+
+    return () => {
+      PhaserGameRef.current?.destroy(true, true);
+      PhaserGameRef.current = null;
+    };
+  }, []);
+
+  return <div id="game-area" className="font-MaplestoryBold"></div>;
 };
 
 export default Phaser;

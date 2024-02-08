ffd8533057da074a24f272d98e1693511a383643
Zena <33304871+Heojiyeon@users.noreply.github.com>
2024-02-08 09:12:58 +0900

    feat: 게임 방법 공지 글 구현

--- src/scenes/notice.ts
+++ src/scenes/notice.ts
@@ -30,6 +30,36 @@ class NoticeScene extends Scene {
       'noticeBoard'
     );
 
+    this.add.text(350, 150, '게임 방법', {
+      fontFamily: 'Maplestory-Light',
+      fontSize: 20,
+      color: '#000000',
+    });
+
+    this.add.text(290, 210, '올바른 알파벳을 획득해주세요.', {
+      fontFamily: 'Maplestory-Light',
+      fontSize: 15,
+      color: '#000000',
+    });
+
+    this.add.text(290, 240, '틀린 알파벳과 씨앗은 닿으면 아파요.', {
+      fontFamily: 'Maplestory-Light',
+      fontSize: 15,
+      color: '#000000',
+    });
+
+    this.add.text(290, 270, '단계 별 악어와 박쥐도 닿으면 아파요.', {
+      fontFamily: 'Maplestory-Light',
+      fontSize: 15,
+      color: '#000000',
+    });
+
+    this.add.text(290, 300, '바나나를 먹으면 힘이 나요! :)', {
+      fontFamily: 'Maplestory-Light',
+      fontSize: 15,
+      color: '#0000FF',
+    });
+
     this.time.delayedCall(3000, () => {
       this.scene.stop('notice-scene');
       this.scene.launch('round1-scene');
